4eab23d290bea2daeb6103e8b86667ebe6883097
Shantanu Pandey <shantanu.pandey@live.in>
2022-09-29 15:27:33 +0530

    Create api.py

---
+++ flaskapidocker/api.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def helloworld():
+    return "<p>Hello! Click below</p> <button>Do Not Click Me</button>"
+
+@app.route('/add/<x>')
+def abc(x):
+    return (x + " bye bye")
+
+app.run(host="0.0.0.0")
